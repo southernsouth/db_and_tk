50eacca13e2e1f3256e49066df67b4bb6b05fa4c
southernsouth <krasnikovblabla@gmail.com>
2022-09-21 17:41:29 +0300

    Переіменування змінних

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from tkinter import *
 from tkinter import messagebox
 
 def button1():
-    if e1.get() != '' and e2.get() != '' and e3.get() != '':
-        cursor.execute('INSERT INTO tk_text (text1, text2, text3) VALUES ("{}", "{}", "{}")'.format(e1.get(), e2.get(), e3.get()))
+    if entry_1.get() != '' and entry_2.get() != '' and entry_3.get() != '':
+        cursor.execute('INSERT INTO tk_text (text1, text2, text3) VALUES ("{}", "{}", "{}")'.format(entry_1.get(), entry_2.get(), entry_3.get()))
         con.commit()
     else:
         messagebox.showwarning('Information', 'The line is empty.')
@@ -14,23 +14,23 @@ def button2():
     cursor.execute('SELECT * FROM tk_text')
     data = cursor.fetchall()
 
-    if e4.get() != '' and e4.get().isdigit() != False and int(e4.get()) > 0 and int(e4.get()) <= len(data):
-        l3['text'] = '{} {} {}'.format((data[int(e4.get()) - 1])[1], (data[int(e4.get()) - 1])[2], (data[int(e4.get()) - 1])[3])
+    if entry_4.get() != '' and entry_4.get().isdigit() != False and int(entry_4.get()) > 0 and int(entry_4.get()) <= len(data):
+        label_3['text'] = '{} {} {}'.format((data[int(entry_4.get()) - 1])[1], (data[int(entry_4.get()) - 1])[2], (data[int(entry_4.get()) - 1])[3])
     else:
         messagebox.showwarning('Information', 'The line is empty\nor invalid input.')
 
 def button3():
     cursor.execute('SELECT * FROM tk_text')
     data = cursor.fetchall()
-    x = ''
+    result = ''
 
     for i in range(len(data)):
-        x += '{}. {} {} {}\n'.format((data[i])[0], (data[i])[1], (data[i])[2], (data[i])[3])
-    x.rstrip()
+        result += '{}. {} {} {}\n'.format((data[i])[0], (data[i])[1], (data[i])[2], (data[i])[3])
+    result.rstrip()
     
-    t1.config(state=NORMAL)
-    t1.delete(1.0, tk.END)
-    t1.insert(tk.END, x)
+    text_1.config(state=NORMAL)
+    text_1.delete(1.0, tk.END)
+    text_1.insert(tk.END, result)
 
 con = mysql.connector.connect(host='localhost',
                                 database='tk',
@@ -39,68 +39,67 @@ con = mysql.connector.connect(host='localhost',
 
 cursor = con.cursor()
 
-w = tk.Tk()
-w.title('Database')
-w.minsize(width=200, height=500)
-w.maxsize(width=200, height=500)
+window = tk.Tk()
+window.title('Database')
+window.minsize(width=200, height=500)
+window.maxsize(width=200, height=500)
 
-f1 = tk.Frame(w)
-f1.pack()
+frame_1 = tk.Frame(window)
+frame_1.pack()
 
-l_ = tk.Label(w, text='')
-l_.pack()
+label_space = tk.Label(window, text='')
+label_space.pack()
 
-f2 = tk.Frame(w)
-f2.pack()
+frame_2 = tk.Frame(window)
+frame_2.pack()
 
-l_2 = tk.Label(w, text='')
-l_2.pack()
+label_space.pack()
 
-f3 = tk.Frame(w)
-f3.pack()
-
-
-l1 = tk.Label(f1, text='Add to database:')
-l1.pack()
-
-e1 = tk.Entry(f1, bg='grey', fg='white')
-e1.pack()
-
-e2 = tk.Entry(f1, bg='grey', fg='white')
-e2.pack()
-
-e3 = tk.Entry(f1, bg='grey', fg='white')
-e3.pack()
-
-b1 = tk.Button(f1, text='Add', width=10, height=1, command=button1)
-b1.pack()
+frame_3 = tk.Frame(window)
+frame_3.pack()
 
 
-l2 = tk.Label(f2, text='Show by id:')
-l2.pack()
+label_1 = tk.Label(frame_1, text='Add to database:')
+label_1.pack()
 
-e4 = tk.Entry(f2, bg='grey', fg='white')
-e4.pack()
+entry_1 = tk.Entry(frame_1, bg='grey', fg='white')
+entry_1.pack()
 
-b2 = tk.Button(f2, text='Show', width=10, height=1, command=button2)
-b2.pack()
+entry_2 = tk.Entry(frame_1, bg='grey', fg='white')
+entry_2.pack()
 
-l3 = tk.Label(f2, text='')
-l3.pack()
+entry_3 = tk.Entry(frame_1, bg='grey', fg='white')
+entry_3.pack()
+
+button_1 = tk.Button(frame_1, text='Add', width=10, height=1, command=button1)
+button_1.pack()
 
 
-l4 = tk.Label(f3, text='Show all:')
-l4.pack()
+label_2 = tk.Label(frame_2, text='Show by id:')
+label_2.pack()
 
-t1 = tk.Text(f3, width=21, height=10, wrap='word')
-scrollbar = Scrollbar(f3, orient=VERTICAL, command=t1.yview)
+entry_4 = tk.Entry(frame_2, bg='grey', fg='white')
+entry_4.pack()
+
+button_2 = tk.Button(frame_2, text='Show', width=10, height=1, command=button2)
+button_2.pack()
+
+label_3 = tk.Label(frame_2, text='')
+label_3.pack()
+
+
+label_4 = tk.Label(frame_3, text='Show all:')
+label_4.pack()
+
+text_1 = tk.Text(frame_3, width=21, height=10, wrap='word')
+scrollbar = Scrollbar(frame_3, orient=VERTICAL, command=text_1.yview)
 scrollbar.pack(side='right', fill='y')
-t1.configure(yscrollcommand=scrollbar.set)
-t1.pack()
+text_1.configure(yscrollcommand=scrollbar.set)
+text_1.pack()
 
-b3 = tk.Button(f3, text='Show', width=10, height=1, command=button3)
-b3.pack()
+button_3 = tk.Button(frame_3, text='Show', width=10, height=1, command=button3)
+button_3.pack()
 
-w.mainloop()
+window.mainloop()
 
 cursor.close()
